f88ba32dc4429c17f08479e4fe7512baa9ded368
Huntyr Wright <gaminggyrrl@gmail.com>
2021-10-05 16:16:46 -0700

    Can now rotate cue stick around cue ball

--- src/Stick.cpp
+++ src/Stick.cpp
@@ -1,11 +1,35 @@
 #include "Stick.h"
 
+#include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+#include <math.h>
 
-void Stick::SetPosition(float x, float y)
+#include <iostream>
+
+void Stick::SetStartPosition(float x, float y)
 {
 	m_Pos.x = x;
 	m_Pos.y = y;
 
-	SetModelMatrix(glm::translate(glm::mat4(1.0f), glm::vec3(x, y, 0)));
+	m_StartPos = m_Pos;
+
+	m_Model = glm::translate(glm::mat4(1.0f), glm::vec3(x, y, 0));
+}
+
+void Stick::RotateStick(float x, float y, glm::vec2 pivotPoint)
+{
+	float theta = atan2f(y, x);
+
+	float cosine = cos(theta);
+	float sine = sin(theta);
+	float cx = pivotPoint.x;
+	float cy = pivotPoint.y;
+	
+	// rotate stick according to the desired distance between the ball and the stick
+	m_Pos.x = ((m_StartPos.x - cx) * cosine - (m_StartPos.y - cy) * sine) + cx;
+	m_Pos.y = ((m_StartPos.x - cx) * sine + (m_StartPos.y - cy) * cosine) + cy;
+
+	// update the model matrix
+	m_Model = glm::translate(glm::mat4(1.0f), glm::vec3(m_Pos.x, m_Pos.y, 0));
+	m_Model *= glm::rotate(glm::mat4(1.0f), theta, glm::vec3(0.0f, 0.0f, 1.0f));
 }
