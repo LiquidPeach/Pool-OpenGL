2d6f84810d94f95e6a1a4dc7e10e93ac68454725
Huntyr Wright <gaminggyrrl@gmail.com>
2021-10-28 21:51:40 -0700

    Added files for collision algorithm

---
+++ src/Collision.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Ball.h"
+#include <vector>
+#include <memory>
+
+class Collision { // Sweep and Prune algorithm
+
+public:
+	Collision() = default;
+	~Collision() = default;
+};
